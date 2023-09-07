d56ddf97c8e75d67f2e2bb5e692402283e0b1f47
bigyan-sapkota <bigyansapkotavines@gmail.com>
2023-09-07 23:53:57 +0545

    making todolist form

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 const page = () => {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
   return (
     <>
       <h1 className="bg-black text-white p-5 text-2xl font-bold text-center">
@@ -10,8 +13,22 @@ const page = () => {
         <input
           type="text"
           className="text-2xl border-zinc-800 border-4 m-5 px-4 py-2"
-          placeholder="Enter Task Here.."
+          placeholder="Enter Title Here.."
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
         />
+
+        <input
+          type="text"
+          className="text-2xl border-zinc-800 border-4 m-5 px-4 py-2"
+          placeholder="Enter Description Here.."
+        />
+
+        <button className="bg-black text-white px-4 py-2 text-2xl font-bold rounded m-5">
+          Add Task
+        </button>
       </form>
     </>
   );
